2e302a126ec9800b924c77326c13d8890c915949
elisey654321 <elisey654321@mail.ru>
2022-07-31 22:44:00 +0300

    Upgrade.

--- src/main/java/com/compile/testcompilerspring/data/UsersChat.java
+++ src/main/java/com/compile/testcompilerspring/data/UsersChat.java
@@ -1,8 +1,10 @@
 package com.compile.testcompilerspring.data;
 
 import lombok.*;
+import org.hibernate.Session;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Data
@@ -20,5 +22,24 @@ public class UsersChat {
     @OneToOne
     @JoinColumn(name = "user_id")
     User user;
+
+    @Getter
+    @Transient
+    static String contact = "<div onclick=\"clickDiv(this.id)\" id=\"numberContact\">\n" +
+            "      <p>name</p>\n" +
+            "    </div>";
+    /*
+    get array contacts
+     */
+    public static ArrayList<UsersChat> getArrayListContacts(User nowUser, Session session){
+        ArrayList<UsersChat> usersChats = (ArrayList<UsersChat>) session.createSQLQuery(getSqlQuerry()).addEntity(UsersChat.class).list();
+
+
+        return new ArrayList<>();
+    }
+
+    private static String getSqlQuerry() {
+        return "select * from user";
+    }
 
 }
