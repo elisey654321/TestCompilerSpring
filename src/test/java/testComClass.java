6d7b20c91f981ab78ea53533ce5d8af7ffa6260a
elisey654321 <elisey654321@mail.ru>
2022-08-04 23:57:58 +0300

    Test comconncetor java.

--- src/test/java/testComClass.java
+++ src/test/java/testComClass.java
@@ -8,27 +8,29 @@ import org.junit.jupiter.api.Test;
 public class testComClass {
     @Test
     void testCom(){
-        System.out.println("test");
+//        System.out.println("test");
 
-        ActiveXComponent v8App = new ActiveXComponent("V83.14.COMConnector");
-        String connectionString = "rvr=\"\"srv-an\"\";Ref=\"\"upn\"\";Usr=\"\"admin\"\";Pwd=\"\"\"\";";
+        System.out.println(System.getProperty("java.library.path"));
+//        System.loadLibrary("jacob-1.20-x64.dll");
+
+
+        ActiveXComponent v8App = new ActiveXComponent("V83.ComConnector");
+        String connectionString = "Srvr=\"srv-an\";Ref=\"upn\";Usr=\"admin\";Pwd=\"\";";
 
         try {
             Variant isConnected = Dispatch.call(v8App, "Connect", connectionString);
 
+            Dispatch query = (Dispatch) Dispatch.call(v8App, "NewObject", "Query").getDispatch();
+            Dispatch.put(query, "Text", "SELECT Items.Ref,Items.name FROM Catalog.ОбъектыСтроительства AS Items");
+            Dispatch result = Dispatch.call(query, "Execute").toDispatch();
+            Dispatch select = Dispatch.call(result, "Choose").toDispatch();
 
+            String itemDescription = "";
 
-//            Dispatch query = (Dispatch) Dispatch.call(v8App, "NewObject", "Query").getDispatch();
-//            Dispatch.put(query, "Text", "SEL ECT Items.Ref, Items.Description FR OM Catalog.Номенклатура AS Items");
-//            Dispatch result = Dispatch.call(query, "Execute").toDispatch();
-//            Dispatch select = Dispatch.call(result, "Choose").toDispatch();
-//
-//            String itemDescription = "";
-//
-//            while (Dispatch.call(select, "Next").getBoolean()) {
-//                itemDescription = Dispatch.get(select, "Description").getString();
-//                System.out.println(itemDescription);
-//            }
+            while (Dispatch.call(select, "Next").getBoolean()) {
+                itemDescription = Dispatch.get(select, "name").getString();
+                System.out.println(itemDescription);
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
