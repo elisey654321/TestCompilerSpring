c104371a811fe4ec57cde4a73b19c7aef7b0e568
elisey654321 <elisey654321@mail.ru>
2022-08-09 11:59:09 +0300

    test

---
+++ src/main/java/com/compile/testcompilerspring/games/Games.java
@@ -0,0 +1,14 @@
+package com.compile.testcompilerspring.games;
+
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+
+public class Games {
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.println("test");
+        }
+
+    }
+}
