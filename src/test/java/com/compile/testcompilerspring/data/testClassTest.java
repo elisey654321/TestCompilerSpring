6e4cc8687fca94c697aa40f92572cc2f57e0d059
elisey654321 <elisey654321@mail.ru>
2022-08-10 22:03:57 +0300

    test mechanism

--- src/test/java/com/compile/testcompilerspring/data/testClassTest.java
+++ src/test/java/com/compile/testcompilerspring/data/testClassTest.java
@@ -2,12 +2,36 @@ package com.compile.testcompilerspring.data;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 class TestClassTest {
 
+//    @Test
+//    void checkClass (){
+//        Optional<TestClass> testClass = Optional.of(TestClass.builder().id(5).build());
+//
+//        testClass.orElseThrow(() -> {
+//            new NoSuchElementException(
+//                    "Book is not found"
+//            );
+//            return null;
+//        });
+//
+//        TestClass tc = TestClass.builder().id(5).build();
+//        System.out.println(tc.getId());
+//    }
+
     @Test
-    void checkClass(){
-        TestClass tc = TestClass.builder().id(5).build();
-        System.out.println(tc.getId());
+    void optionalTest(){
+        Optional<String> optionalS = Optional.of("test");
+        optionalS.orElse(orElse());
+        System.out.println(optionalS.get());
+    }
+
+    String orElse(){
+        System.out.println("no no no no");
+        return "test1";
     }
 
 }
