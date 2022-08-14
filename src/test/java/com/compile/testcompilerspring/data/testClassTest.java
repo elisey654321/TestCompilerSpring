34317bebbe530acd445cd31fe953887ecebffce6
elisey654321 <elisey654321@mail.ru>
2022-08-14 15:38:04 +0300

    tested

--- src/test/java/com/compile/testcompilerspring/data/testClassTest.java
+++ src/test/java/com/compile/testcompilerspring/data/testClassTest.java
@@ -1,37 +1,66 @@
 package com.compile.testcompilerspring.data;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import javax.persistence.Entity;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.*;
+import java.util.function.Function;
 
 class TestClassTest {
 
-//    @Test
-//    void checkClass (){
-//        Optional<TestClass> testClass = Optional.of(TestClass.builder().id(5).build());
-//
-//        testClass.orElseThrow(() -> {
-//            new NoSuchElementException(
-//                    "Book is not found"
-//            );
-//            return null;
-//        });
-//
-//        TestClass tc = TestClass.builder().id(5).build();
-//        System.out.println(tc.getId());
-//    }
-
     @Test
-    void optionalTest(){
-        Optional<String> optionalS = Optional.of("test");
-        optionalS.orElse(orElse());
-        System.out.println(optionalS.get());
+    void testSet(){
+
+//        HashSet<Integer> eee = new HashSet<>();
+//        eee.add(123);
+//        eee.add(12311);
+//        eee.add(123);
+//
+//        eee.stream().forEach(System.out::println);
+
+//        HashMap<String,Integer> stringIntegerHashMap = new HashMap<>();
+//        stringIntegerHashMap.put("222",123213);
+//        stringIntegerHashMap.put("222111",1123213);
+//
+//        System.out.println(stringIntegerHashMap.get("222"));
+
+        TreeMap<Integer,String> dsad = new TreeMap<>();
+        dsad.put(12321,"tet111");
+        dsad.put(1,"tet33");
+        dsad.put(21,"te11t");
+
+        Set<Integer> keys = dsad.keySet();
+
+//        for (int i = 0; i < keys.size(); i++) {
+            keys.stream().forEach(x -> System.out.println(dsad.get(x)));
+//        }
+
     }
 
-    String orElse(){
-        System.out.println("no no no no");
-        return "test1";
+    private @NotNull ArrayList<Integer> deleteElementsFromArrayList(@NotNull ArrayList<Integer> list, Integer firstCharacter, Integer howMuchDelete) {
+        howMuchDelete--;
+
+        Object[] test = new Object[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            test[i] = list.get(i);
+        }
+
+        Integer length = test.length;
+        int numMoved = length - firstCharacter - 1;
+        System.arraycopy(test, firstCharacter + 1 + howMuchDelete, test, firstCharacter, numMoved - howMuchDelete);
+
+        list = new ArrayList<>();
+
+        for (int i = 0; i < test.length - howMuchDelete - 1; i++) {
+            Optional<Object> o = Optional.ofNullable(test[i]);
+            if (o.get() instanceof Integer){
+                list.add((Integer) o.get());
+            }
+        }
+        return list;
     }
+
 
 }
